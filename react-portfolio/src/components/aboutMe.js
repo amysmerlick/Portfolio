15f03eff2d6e2eff6fe85c87d847951f9b1c8e29
Amy Smerlick <77814900+amysmerlick@users.noreply.github.com>
2021-07-27 13:56:40 -0400

    updated css to aboutme section

--- react-portfolio/src/components/aboutMe.js
+++ react-portfolio/src/components/aboutMe.js
@@ -5,8 +5,8 @@ const AboutMe = () => {
     return (
         <section id="Biography" className="Biography">
             {/* <h2><u>Biography</u></h2> */}
-            <p>Hello World! My name is Amy Smerlick, a Full Stack web-developer student who is nearing the end of my course and ready to get out and explore this new career change. I am 
-                excited to refine my skills with the right company and look forward to growing in this field. 
+            <p>Hello World! My name is Amy Smerlick, a Full Stack web-development student, who is nearing the end of my course and ready to get out and explore this new career change. I am
+                excited to refine my skills with the right company and look forward to growing in this field. I am the owner of a home infusion business, as well as a licensed Realtor.
             </p>
             <div className="amyshead">
                 <img src="./Images/Headshot.jpg" height="200" alt="headshot" className="headshot" />
